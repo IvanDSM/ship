9c0dc4001ff77a1b9e8fee04579c97f3fad56850
IvanDSM <snesivan1@gmail.com>
2019-03-24 11:56:40 -0300

    Add LevelEvent struct, LevelEventType enum and stub Level::InterpretEvent.

--- level.cpp
+++ level.cpp
@@ -1,5 +1,10 @@
 #include "level.h"
 
+void Level::InterpretEvent(LevelEvent event)
+{
+	//stub
+}
+
 void Level::Tick()
 {
 	y = -720 + (line % 720);
--- level.h
+++ level.h
@@ -3,12 +3,30 @@
 
 #include <object.h>
 
+enum class LevelEventType
+{
+	LEVENT_STARTLEVEL,
+	LEVENT_SPEEDCHANGE,
+	LEVENT_SPAWNENEMY,
+	LEVENT_SETBLINK,
+	LEVENT_ENDLEVEL
+};
+
+struct LevelEvent
+{
+		int				event_line;
+		LevelEventType	event_type;
+		int				value;
+		LevelEvent		*child;
+};
+
 class Level : public Object
 {
 	protected:
 		int				line = 0;
 
 	public:
+		void			InterpretEvent(struct LevelEvent event);
 						Level (Game *Pgame) : Object(Pgame, (char *) "level.png", true, 0, -720)
 						{
 							type = ObjectType::OBJ_LEVEL;
